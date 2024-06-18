7434c89c013b048352c1de7e9d354d5e366e6ecd
ptaylor <paul.e.taylor@me.com>
2024-06-18 15:28:46 -0700

    assets

--- extract.sh
+++ extract.sh
@@ -6,7 +6,6 @@ cd "$( cd "$( dirname "$(realpath -m "${BASH_SOURCE[0]}")" )" && pwd )";
 set -x;
 set -euo pipefail;
 
-if test -f root.zip; then
-    mkdir -p mnt;
-    unzip -o root.zip -d mnt/;
+if test -f customize/assets.zip; then
+    unzip -o customize/assets.zip -d assets;
 fi
